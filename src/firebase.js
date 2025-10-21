4e5f35ad44193306919563a08e3ba045a80a0375
Vinay <vinay83862@gmail.com>
2025-10-21 15:39:19 +0530

    Secured Firebase key using .env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSpYZNNevWXuKHwfd1DgLtzX6FQf1oreU",
-  authDomain: "manager-e49ba.firebaseapp.com",
-  databaseURL: "https://manager-e49ba-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "manager-e49ba",
-  storageBucket: "manager-e49ba.firebasestorage.app",
-  messagingSenderId: "58249200567",
-  appId: "1:58249200567:web:25f59128aa0a7648fede32",
-  measurementId: "G-B3YJ9XW3V0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export default app;
