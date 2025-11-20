c3ebb701c5fef487645ab31a1d85bbf0168dd4cc
Vinay <vinay83862@gmail.com>
2025-11-20 12:06:27 +0530

    Updates

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBwwdS17K742qESzLOUQSI6nNgsdbvQFTE",
+    authDomain: "manager-e49ba.firebaseapp.com",
+    databaseURL: "https://manager-e49ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "manager-e49ba",
+    storageBucket: "manager-e49ba.appspot.com",
+    messagingSenderId: "58249200567",
+    appId: "1:58249200567:web:25f59128aa0a7648fede32",
+    measurementId: "G-B3YJ9XW3V0"
+};
